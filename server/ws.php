fed4694ff78f26c935cffffc14ba046ccd46d545
frank <13732629211@163.com>
2018-09-08 11:46:03 +0800

    在ws.php服务添加task任务

--- server/ws.php
+++ server/ws.php
@@ -6,10 +6,16 @@ class Ws{
     public function __construct()
     {
         $this->ws = new swoole_websocket_server(self::HOST,self::PORT);
+        $this->ws->set([
+            'worker_num' => 4,    //worker process num
+            'task_worker_num' =>100
+        ]);
         // 回调函数的对象写法
         $this->ws->on('open',[$this,'onOpen']);
         $this->ws->on('message',[$this,'onMessage']);
         $this->ws->on('close',[$this,'onClose']);
+        $this->ws->on('task',[$this,'onTask']);
+        $this->ws->on('finish',[$this,'onFinish']);
         $this->ws->start();
     }
     /**
@@ -33,9 +39,41 @@ class Ws{
      */
     public function onMessage($ws,$frame){
         echo "接受到的客户端的数据:{$frame->data}\n";
+        
+        // 投递一个异步任务
+        $data = [
+            'task' => 1,
+            'fd' => $frame->fd
+        ];
+        $ws->task($data);
+
+        // 下面的代码不会等待上面执行完毕再执行
         $ws->push($frame->fd,"服务端推送的数据".date('Y-m-d H:i:s'));
     }
-
+    /**
+     * 监听$ws->task接受一个异步任务
+     * @param [type] $serv
+     * @param [type] $taskId
+     * @param [type] $workerId
+     * @param [type] $data
+     * @return void
+     */
+    public function onTask($serv,$taskId,$workerId,$data){
+        print_r($data);
+        sleep(10);
+        return "任务结束"; // 将结果返回给worker进程  会触发onFinish函数
+    }
+    /**
+     * 当worker进程投递的任务在task_worker中完成时，task进程会通过swoole_server->finish()方法将任务处理的结果发送给worker进程。
+     * @param [type] $serv
+     * @param [type] $taskId
+     * @param [type] $data
+     * @return void
+     */
+    public function onFinish($serv,$taskId,$data){
+        echo "taskId:{$taskId}\n";
+        echo "任务结束后的返回参数:{$data}";
+    }
     /**
      * 监听wx关闭
      *
